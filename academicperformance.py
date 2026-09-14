3aef7c867f953ae79394e48a1e81355b5d505013
Emlhance121218 <ehPahilangco@mcm.edu.ph>
2026-09-14 13:59:25 +0800

    Update academicperformance.py

--- academicperformance.py
+++ academicperformance.py
@@ -1,1 +1,12 @@
+score = float(input("Enter score: "))
 
+if score >= 90:
+    grade = "Excellent"
+elif score >= 75:
+    grade = "Good"
+elif score >= 60:
+    grade = "Satisfactory"
+else:
+    grade = "Needs Improvement"
+
+print(grade)
