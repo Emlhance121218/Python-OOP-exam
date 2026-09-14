c455a6e0a0c6a66a7c3d25054f7d6e3bbf222930
Emlhance121218 <ehPahilangco@mcm.edu.ph>
2026-09-14 13:59:59 +0800

    Update weatherdaychecker.py

--- weatherdaychecker.py
+++ weatherdaychecker.py
@@ -1,1 +1,10 @@
+temperature = float(input("Enter temperature (°C): "))
 
+if temperature >= 35:
+    print("Hot Day")
+elif temperature >= 20:
+    print("Warm Day")
+elif temperature >= 10:
+    print("Cool Day")
+else:
+    print("Cold Day")
